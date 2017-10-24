06d7b9322568bab691aa167b0e0b5d6569b86f56
Aghasy <aghasy@picsart.com>
2017-10-24 09:42:03 +0400

    New model lstmr+fc

--- LSTMRCell.py
+++ LSTMRCell.py
@@ -1,6 +1,50 @@
 import tensorflow as tf
 import tensorflow.contrib as contrib
+from tensorflow.python.ops.rnn_cell_impl import _linear, LSTMStateTuple
 
 
-class LSTMRCell(contrib.rnn.LSTMBlockCell)
-    pass
+class LSTMRCell(contrib.rnn.BasicLSTMCell):
+    def __init__(self, num_units, readonly_memory, forget_bias=1.0, activation=None, reuse=None):
+        super(LSTMRCell, self).__init__(
+            num_units=num_units,
+            forget_bias=forget_bias,
+            state_is_tuple=True,
+            activation=activation,
+            reuse=reuse)
+        self.readonly_memory = readonly_memory
+
+    def __call__(self, inputs, state, scope=None):
+        """Long short-term memory cell with read only unit (LSTM).
+
+            Args:
+              inputs: `2-D` tensor with shape `[batch_size x input_size]`.
+              state: An `LSTMStateTuple` of state tensors, each shaped
+                `[batch_size x self.state_size]`, if `state_is_tuple` has been set to
+                `True`.  Otherwise, a `Tensor` shaped
+                `[batch_size x 2 * self.state_size]`.
+
+            Returns:
+              A pair containing the new hidden state, and the new state (either a
+                `LSTMStateTuple` or a concatenated state, depending on
+                `state_is_tuple`).
+            """
+        sigmoid = tf.sigmoid
+        # Parameters of gates are concatenated into one multiply for efficiency.
+        if self._state_is_tuple:
+            c, h = state
+        else:
+            c, h = tf.split(value=state, num_or_size_splits=2, axis=1)
+
+        concat = _linear([inputs, h], 5 * self._num_units, True)
+
+        # i = input_gate, j = new_input, f = forget_gate, o = output_gate, r = readonly_gate
+        i, j, f, o, r = tf.split(value=concat, num_or_size_splits=5, axis=1)
+
+        new_c = c * sigmoid(f + self._forget_bias) + sigmoid(i) * self._activation(j) + sigmoid(r) * sigmoid(self.readonly_memory)
+        new_h = self._activation(new_c) * sigmoid(o)
+
+        if self._state_is_tuple:
+            new_state = LSTMStateTuple(new_c, new_h)
+        else:
+            new_state = tf.concat([new_c, new_h], 1)
+        return new_h, new_state
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import tensorflow.contrib as contrib
-from tensorflow.python.ops.rnn_cell_impl import LSTMStateTuple
+
+from LSTMRCell import LSTMRCell
 
 
 def im22txt(features, labels, mode, params):
@@ -10,7 +11,7 @@ def im22txt(features, labels, mode, params):
 
     # Image Embedding
     with tf.variable_scope("image_embedding") as scope:
-        image_embeddings = tf.contrib.layers.fully_connected(
+        image_embeddings = contrib.layers.fully_connected(
             inputs=features['features'],
             num_outputs=params['embedding_size'],
             activation_fn=None,
@@ -27,9 +28,17 @@ def im22txt(features, labels, mode, params):
         if mode != tf.estimator.ModeKeys.PREDICT:
             seq_embeddings = tf.nn.embedding_lookup(embedding_map, features['input_seq'])
 
+    with tf.variable_scope("readonly_component") as scope:
+        readonly_component = contrib.layers.fully_connected(
+            inputs=features['features'],
+            num_outputs=params['embedding_size'],
+            activation_fn=None,
+            weights_initializer=initializer,
+            biases_initializer=None,
+            scope=scope)
+
     # LSTM
-    lstm_cell = contrib.rnn.BasicLSTMCell(
-        num_units=params['num_lstm_units'], state_is_tuple=True)
+    lstm_cell = LSTMRCell(num_units=params['num_lstm_units'], readonly_memory=readonly_component)
 
     # Dropout
     if mode == tf.estimator.ModeKeys.TRAIN:
@@ -44,7 +53,6 @@ def im22txt(features, labels, mode, params):
     train_op = None
     total_loss = None
     eval_metric_ops = None
-    beam_size = params['beam_size']
 
     with tf.variable_scope("lstm", initializer=initializer) as lstm_scope:
         # Feed the image embeddings to set the initial LSTM state.
@@ -56,104 +64,11 @@ def im22txt(features, labels, mode, params):
         lstm_scope.reuse_variables()
 
         if mode == tf.estimator.ModeKeys.PREDICT:
-            initial_state = LSTMStateTuple(tf.tile(initial_state[0], [beam_size, 1]),
-                                           tf.tile(initial_state[1], [beam_size, 1]))
-
-            def body(time, pred_tuple, state_tuple):
-                ides, coefs = pred_tuple
-                with tf.variable_scope("seq_embedding"), tf.device("/cpu:0"):
-                    seq_embeddings = tf.nn.embedding_lookup(embedding_map, ides[-1])
-
-                # Run a single LSTM step.
-                lstm_outputs, state_tuple = lstm_cell(
-                    inputs=seq_embeddings,
-                    state=state_tuple)
-
-                # lstm_outputs = tf.reshape(lstm_outputs, [-1, lstm_cell.output_size])
-
-                # TODO: Remove scope hacking by replacing contrib.layers.fully_connected with custom implantation
-                def custom_getter(getter, name, *args, **kwargs):
-                    kwargs = kwargs.copy()
-                    kwargs['reuse'] = None
-                    name = name.replace('lstm/', '')
-                    return getter(name, *args, **kwargs)
-
-                with tf.variable_scope("logits", custom_getter=custom_getter) as logits_scope:
-                    logits = contrib.layers.fully_connected(
-                        inputs=lstm_outputs,
-                        num_outputs=params['vocab_size'],
-                        activation_fn=None,
-                        weights_initializer=initializer,
-                        scope=logits_scope)
-
-                prediction = tf.nn.softmax(logits, name="softmax")
-
-                coef, w_i = tf.nn.top_k(prediction, k=beam_size)
-
-                def repeat(x, n, shape):
-                    return tf.reshape(tf.tile(tf.reshape(x, [-1, 1]), [1, n]), shape)
-
-                coefs = repeat(coefs, beam_size, [beam_size*beam_size])
-
-                all_coefs = coefs + tf.log(tf.reshape(coef, [-1]))
-                all_seq_i = tf.transpose(tf.concat([repeat(ides, beam_size, [-1, beam_size*beam_size]),
-                                                    tf.reshape(w_i, [1, -1])], axis=0))
-
-                all_unique_coefs, idx = tf.unique(all_coefs)
-                all_unique_seq_i = tf.gather(all_seq_i, tf.unique(idx)[0])
-
-                unique_cond = tf.equal(time, 0)
-
-                all_seq_i = tf.case([(unique_cond, lambda: all_unique_seq_i)],
-                                    default=lambda: all_seq_i)
-
-                all_coefs = tf.case([(unique_cond, lambda: all_unique_coefs)],
-                                    default=lambda: all_coefs)
-
-                top_cofs, top_id = tf.nn.top_k(all_coefs, k=beam_size)
-
-                s_i = tf.gather(all_seq_i, top_id)
-                with tf.control_dependencies([
-                   # tf.Print(ides, [time + 1, tf.transpose(s_i), top_cofs], summarize=1000)
-                ]):
-                    return time + 1, (tf.transpose(s_i), top_cofs), state_tuple
-
-            def cond(i, pred_tuple, _):
-                ides, _ = pred_tuple
-                cond_1 = i < params['seq_max_len']
-                end_words = [params['end_word_index']] * beam_size
-                cond_2 = tf.reduce_any(tf.not_equal(ides[i], end_words))
-                with tf.control_dependencies([
-                    # tf.Print(ides, [ides[i], cond_2], summarize=1000)
-                ]):
-                    return tf.logical_and(cond_1, cond_2)
-
-            with tf.control_dependencies([tf.assert_equal(1, tf.shape(features['features'])[0],
-                                                          message='in PREDICT batch_size=1')]):
-                i = tf.constant(0, dtype=tf.int32, name='i')
-                initial_pred = (
-                    tf.constant([
-                        [params['start_word_index']] * beam_size
-                    ], dtype=tf.int32, name='word_indexes'),
-                    tf.constant([0.0] * beam_size, dtype=tf.float32, name='seq_coef'))
-
-                seq_len, (ides, coefs), _ = tf.while_loop(cond=cond,
-                                                          body=body,
-                                                          loop_vars=[i, initial_pred, initial_state],
-                                                          shape_invariants=[
-                                                              i.shape,
-                                                              (tf.TensorShape([None, beam_size]),
-                                                               initial_pred[1].shape),
-                                                              LSTMStateTuple(initial_state[0].shape,
-                                                                             initial_state[1].shape)
-                                                          ],
-                                                          parallel_iterations=1,
-                                                          back_prop=False)
-
-            with tf.control_dependencies([
-                # tf.Print(ides, [ides, coefs], summarize=1000)
-            ]):
-                predict_res = {'ides': tf.expand_dims(ides, 0), 'coef': tf.expand_dims(coefs, 0)}
+            predict_res = predict_loop(lstm_cell=lstm_cell,
+                                       embedding_map=embedding_map,
+                                       initializer=initializer,
+                                       initial_state=initial_state,
+                                       features=features['features'])
 
         else:
             # dynamic_rnn
@@ -232,8 +147,114 @@ def im22txt(features, labels, mode, params):
                 clip_gradients=params['clip_gradients'],
                 learning_rate_decay_fn=learning_rate_decay_fn)
 
+            for var in tf.trainable_variables():
+                tf.summary.histogram(var.name, var)
+
     return tf.estimator.EstimatorSpec(mode=mode,
                                       predictions=predict_res,
                                       loss=total_loss,
                                       train_op=train_op,
                                       eval_metric_ops=eval_metric_ops)
+
+
+def predict_loop(lstm_cell, embedding_map, initializer, initial_state, params, features):
+    beam_size = params['beam_size']
+    initial_state = contrib.rnn.LSTMStateTuple(tf.tile(initial_state[0], [beam_size, 1]),
+                                               tf.tile(initial_state[1], [beam_size, 1]))
+
+    def body(time, pred_tuple, state_tuple):
+        ides, coefs = pred_tuple
+        with tf.variable_scope("seq_embedding"), tf.device("/cpu:0"):
+            seq_embeddings = tf.nn.embedding_lookup(embedding_map, ides[-1])
+
+        # Run a single LSTM step.
+        lstm_outputs, state_tuple = lstm_cell(
+            inputs=seq_embeddings,
+            state=state_tuple)
+
+        # lstm_outputs = tf.reshape(lstm_outputs, [-1, lstm_cell.output_size])
+
+        # TODO: Remove scope hacking by replacing contrib.layers.fully_connected with custom implantation
+        def custom_getter(getter, name, *args, **kwargs):
+            kwargs = kwargs.copy()
+            kwargs['reuse'] = None
+            name = name.replace('lstm/', '')
+            return getter(name, *args, **kwargs)
+
+        with tf.variable_scope("logits", custom_getter=custom_getter) as logits_scope:
+            logits = contrib.layers.fully_connected(
+                inputs=lstm_outputs,
+                num_outputs=params['vocab_size'],
+                activation_fn=None,
+                weights_initializer=initializer,
+                scope=logits_scope)
+
+        prediction = tf.nn.softmax(logits, name="softmax")
+
+        coef, w_i = tf.nn.top_k(prediction, k=beam_size)
+
+        def repeat(x, n, shape):
+            return tf.reshape(tf.tile(tf.reshape(x, [-1, 1]), [1, n]), shape)
+
+        coefs = repeat(coefs, beam_size, [beam_size * beam_size])
+
+        all_coefs = coefs + tf.log(tf.reshape(coef, [-1]))
+        all_seq_i = tf.transpose(tf.concat([repeat(ides, beam_size, [-1, beam_size * beam_size]),
+                                            tf.reshape(w_i, [1, -1])], axis=0))
+
+        all_unique_coefs, idx = tf.unique(all_coefs)
+        all_unique_seq_i = tf.gather(all_seq_i, tf.unique(idx)[0])
+
+        unique_cond = tf.equal(time, 0)
+
+        all_seq_i = tf.case([(unique_cond, lambda: all_unique_seq_i)],
+                            default=lambda: all_seq_i)
+
+        all_coefs = tf.case([(unique_cond, lambda: all_unique_coefs)],
+                            default=lambda: all_coefs)
+
+        top_cofs, top_id = tf.nn.top_k(all_coefs, k=beam_size)
+
+        s_i = tf.gather(all_seq_i, top_id)
+        with tf.control_dependencies([
+            # tf.Print(ides, [time + 1, tf.transpose(s_i), top_cofs], summarize=1000)
+        ]):
+            return time + 1, (tf.transpose(s_i), top_cofs), state_tuple
+
+    def cond(i, pred_tuple, _):
+        ides, _ = pred_tuple
+        cond_1 = i < params['seq_max_len']
+        end_words = [params['end_word_index']] * beam_size
+        cond_2 = tf.reduce_any(tf.not_equal(ides[i], end_words))
+        with tf.control_dependencies([
+            # tf.Print(ides, [ides[i], cond_2], summarize=1000)
+        ]):
+            return tf.logical_and(cond_1, cond_2)
+
+    with tf.control_dependencies([tf.assert_equal(1, tf.shape(features)[0],
+                                                  message='in PREDICT batch_size=1')]):
+        i = tf.constant(0, dtype=tf.int32, name='i')
+        initial_pred = (
+            tf.constant([
+                [params['start_word_index']] * beam_size
+            ], dtype=tf.int32, name='word_indexes'),
+            tf.constant([0.0] * beam_size, dtype=tf.float32, name='seq_coef'))
+
+        seq_len, (ides, coefs), _ = tf.while_loop(cond=cond,
+                                                  body=body,
+                                                  loop_vars=[i, initial_pred, initial_state],
+                                                  shape_invariants=[
+                                                      i.shape,
+                                                      (tf.TensorShape([None, beam_size]),
+                                                       initial_pred[1].shape),
+                                                      contrib.rnn.LSTMStateTuple(initial_state[0].shape,
+                                                                                 initial_state[1].shape)
+                                                  ],
+                                                  parallel_iterations=1,
+                                                  back_prop=False)
+
+    with tf.control_dependencies([
+        # tf.Print(ides, [ides, coefs], summarize=1000)
+    ]):
+        predict_res = {'ides': tf.expand_dims(ides, 0), 'coef': tf.expand_dims(coefs, 0)}
+        return predict_res
