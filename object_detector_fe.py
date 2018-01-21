23c4727ee85d195e47ae28ce73836b94c9cdba24
Aghasy <aghasy@picsart.com>
2018-01-21 23:58:02 +0400

    add predict code

--- object_detector_fe.py
+++ object_detector_fe.py
@@ -19,7 +19,18 @@ class ObjectDetectorFE(DownloadableFeatureExtractor):
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='', input_map={'image_tensor:0': images})
-        return tf.reshape(self.feature_selector(), [tf.shape(images)[0], -1]),
+        return tf.reshape(self.feature_selector(), [tf.shape(images)[0], -1]), {
+            'detection_scores': tf.get_default_graph().get_tensor_by_name('detection_scores:0'),
+            'detection_classes': tf.get_default_graph().get_tensor_by_name('detection_classes:0'),
+            'num_detections': tf.get_default_graph().get_tensor_by_name('num_detections:0'),
+            'detection_boxes': tf.get_default_graph().get_tensor_by_name('detection_boxes:0')
+        }
+
+    def output_constructor(self, pred, out_dic):
+        out_dic['detection_scores'] = pred['detection_scores'].tolist()
+        out_dic['detection_classes'] = pred['detection_classes'].tolist()
+        out_dic['num_detections'] = pred['num_detections'].tolist()
+        out_dic['detection_boxes'] = pred['detection_boxes'].tolist()
 
     def load(self, sess: tf.Session):
         pass
